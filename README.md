6bca8b82ff93c4f55a7c437f7f2506ab1760cb9b
luisucan <luis_alberto_ucan@hotmail.com>
2019-05-30 11:23:59 -0500

    a

---
+++ README.md
@@ -0,0 +1,18 @@
+
+#Socket-Server
+Reconstruir modulos de node
+```
+npm install
+```
+
+
+Generar el dist
+```
+tsc -w
+```
+
+levantar el servidor
+```
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,21 @@
+import { Server } from "./clases/server";
+import { SERVER_PORT } from "./global/enviroment";
+import { router } from "./routes/router";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const server = new Server();
+
+///BODY PARSER
+server.app.use( bodyParser.urlencoded({extended:true}) );
+server.app.use( bodyParser.json() );
+
+//CORS
+server.app.use( cors({origin: true, credentials: true}) );
+
+//RUTAS Y SERVICIOS
+server.app.use('/',router);
+
+server.start( ()=>{
+    console.log(`servidor corriendo en el puerto ${SERVER_PORT} `);
+});
